4acf2eb7a93cba5c8fa1739619b93ccd0d0dda6c
SPENCER LORD <spencer@voicebase.com>
2016-01-30 12:31:58 -0800

    Incremental improvements

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 var express = require('express');
 var bodyParser = require("body-parser");
-var multer = require('multer');
 var app = express();
-
-var request = require('request');
-var FormData = require('form-data');
 var fs = require('fs');
 
-//for calling shell script
+var request = require('request');
 var sys = require('sys')
 var exec = require('child_process').exec;
 
-//Here we are configuring express to use body-parser as middle-ware.
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//app.use(multer);
+app.use(express.static('public'));
 
-
-//var pass='letmeon';
-//var url='https://api.dev.voicebase.com/services';
-//var key='2A425147-7577-9542-3AED-4BCF806BB6EE';
-
-var key='1586E668-F431-C9D6-D68B-88641B5C3EB8'
-var pass='mrneutron';
-var url=  "https://api.voicebase.com/services";
-var callback="http://requestb.in/13o1igh1";
-var eCallback="http://requestb.in/13o1igh1";
+var pass='letmeon';
+var url='https://api.dev.voicebase.com/services';
+var key='2A425147-7577-9542-3AED-4BCF806BB6EE';
+//var key='1586E668-F431-C9D6-D68B-88641B5C3EB8'
+//var pass='mrneutron';
+//var url=  "https://api.voicebase.com/services";
+//var callback="http://requestb.in/13o1igh1";
+//var eCallback="http://requestb.in/13o1igh1";
 var scratchDir = '/tmp/';
 
-app.use(express.static('public'));
-//app.use('/static', express.static(__dirname + '/public'));
+//Workaround to have thos global because I can not retriee the callback params :(
+var mediaId;
+var srt;
+var fileName="putThatCoffeeDown.mp4";
+
+
+//io.on('connection', function (socket) {
+  //socket.emit('news', { hello: 'world' });
+  //socket.on('my other event', function (data) {
+    //console.log(data);
+  //});
+//});
+
 
 app.get('/', function(req, res){
   console.log(__dirname)
@@ -37,25 +44,39 @@ app.get('/', function(req, res){
 });
 
 
-app.get("/generatecc", function (req, res) {
-    //var fullUrl = req.protocol + '://' + req.get('host') + '/callback';
+app.get("/cc", requestSRT); 
+
+function requestSRT(req, res) {
+    var fullUrl = req.protocol + '://' + req.get('host') + '/callback';
     //var fullUrl='http://requestb.in/uka2cruk';
-    var fullUrl = 'http://73.252.201.186:3000/callback';
-    var file = 'public/CaddyshackCinderella.mp4';
+    //var fullUrl = 'http://73.252.201.186:3000/callback';
+    var file = 'public/'+fileName;
     //var audioFile = extractAudio(scratchDir,file);
     //uploadFile(audioFile,fullUrl,fullUrl);
-    uploadFile(file,fullUrl,fullUrl);
-    console.log('works, '+fullUrl);
-    res.writeHead(200, {'Content-Type': 'text/plain' });
-    res.end('getting srt');
-});
+    uploadFile(file, fullUrl,fullUrl, res, afterUpload);
+    //var data = {"mediaId":"xxxx","requestStatus":"SUCCESS","fileUrl":"xxxxx"};
+    //res.json(data);
+    //res.send(data);
+}
+
+function afterUpload(err, res, data) {
+    //var data = {"mediaId":"xxxx","requestStatus":"SUCCESS","fileUrl":"xxxxx"};
+    res.json(data);
+}
 
 
 app.post('/callback', function(req, res){
-    //var path = url.parse(req.url).pathname;
-    console.log('Callback, mediaId: ' + req.body.mediaId);
-    res.writeHead(200, {'Content-Type': 'text/plain' });
-    res.end('received callback');
+    var d = new Date();
+    var n = d.toLocaleTimeString();
+    console.log('a callback was received at : ' + n);
+    //TODO:  Can not get the 3 post parameters.  Not sure why
+    //console.log('state : ' + req.body.state);
+    //console.log('mediaId : ' + req.body.mediaId);
+    //`console.log('time charged : ' + req.body.timeCharged);
+
+   // get the closed caption SRT file and write it to the public folder so it can be used in the video    
+   getSRT(mediaId);
+
 });
 
 app.listen(3000, function () {
@@ -63,7 +84,7 @@ app.listen(3000, function () {
 });
 
 
-function uploadFile(file,callback,eCallback) {
+function uploadFile(file,callbackUrl,eCallbackUrl,res,callback) {
 
    var formData = {apiKey: key,
                    password: pass,
@@ -71,8 +92,35 @@ function uploadFile(file,callback,eCallback) {
                    action: 'uploadMedia',
                    transcriptType: 'machine-best',
                    file: fs.createReadStream(file),
-                   machineReadyCallback:callback,
-                   errorCallback:eCallback}
+                   machineReadyCallback:callbackUrl,
+                   errorCallback:eCallbackUrl}
+   request.post({
+         url: url,
+         json: true,
+         formData: formData
+         }, function(err, httpResponse, body) {
+               if (err) {
+                  return console.error('upload failed:', err);
+               }
+              console.log('Upload successful!  Server responded with:', body);
+              for (var prop in body ) {
+                  console.log( prop + ' is ' + body[prop] );
+              }
+              mediaId=body.mediaId;
+              callback(err, res, body);
+        }
+    );
+}
+
+function getSRT(mediaId) {
+
+   var formData = {apiKey: key,
+                   password: pass,
+                   version: '1.1',
+                   action: 'getTranscript',
+                   format: 'srt',
+                   mediaId: mediaId,
+                   content:'True'}
    request.post({
          url: url,
          formData: formData
@@ -81,6 +129,13 @@ function uploadFile(file,callback,eCallback) {
                   return console.error('upload failed:', err);
                }
               console.log('Upload successful!  Server responded with:', body);
+              srt = body;
+              fs.writeFile("public//captions-en.srt", srt, function(err) {
+                  if(err) {
+                      return console.log(err);
+                  }
+                  console.log("The file was saved!");
+              }); 
         }
     );
 }
@@ -90,11 +145,6 @@ function extractAudio(scratchDir,file) {
    var audioFile = scratchDir+generateUUID()+ext;
    var cmd = 'ffmpeg -y -i '+file+ ' -vn -acodec copy '+audioFile;
    console.log(cmd);
-
-   //var history = child_process.execSync('git log', { encoding: 'utf8' });
-   //process.stdout.write(history);
-
-
    var child;
    child = exec(cmd, function (error, stdout, stderr) {
       console.log('stdout: ' + stdout);
