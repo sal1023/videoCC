001478561739976d482d56c1c206fd8d34a59d2e
SPENCER LORD <spencer@voicebase.com>
2016-02-04 14:41:27 -0800

    Added readme, nconf and migrated to v2 api

---
+++ README.md
@@ -0,0 +1,106 @@
+# Sample App to get machine generated closed captions (SRT) using voicebase API
+
+## Overview
+
+This is a simple video steaming app that allows you to explore the features provide by VoiceBase REST APPIs for machine generatd transcripts and closed captions.
+	
+   * for demo purposes videos are streamed from node.  All files in public folder are streamable.
+   * Simple home page with links to each file in public folder
+   * player page uses JWPlayer to stream the file selected
+   * player page has "generate closed caption" button, that will use the VoiceBase API to get an SRT for the video and copy to the demo server so that it can be displayed  in jwplayer 
+
+## Prerequisites
+
+   * ffmpeg should be insalled and in path
+   * A VoiceBase API account
+   * Machine that runs this app must receive a callback from voicebase when transcript is ready, so it must have a public IP
+   
+## Setting up the app
+
+   * Run `npm install` at the root folder to download dependencies.
+   * install ffmpeg if you do not already have it installed
+   * copy one or more mp4 files to public folder 
+   * Run `node app.js --vbtoken <your-token-here>` to start the application.
+   * Navigate to http://localhost:3000/ or http://yourserver:3000/  in your favourite browser.
+
+## Configuration
+
+There are 4 config variables
+
+   * baseurl: Thie baseurl is for the VoiceBase REST API and the default value should be fine as is. 
+   * vbtoken: You will need to get your own VoiceBase Bearer token and specify it either as command line (--vbtoken <your-token-here>) or set an env var of same name 
+   * callback: If the machine you are running this one does not have a public IP, it is possible to use port forwarding but then you might also have to override the callback url.  The demo app will use the same base url that you use for the web app for the callback.   If you are port forwarding, you may need to use a local IP for your browser and then override the url with the public url for the callback.
+   * scratchDir: The scratchdir is where tmp files will be placed and is set to /tmp as a default
+   
+## References 
+
+   * VoiceBase REST API (add link)
+
+
+## Code Example
+
+```javascript
+function uploadFileV2(file, externalId, callbackUrl, res, callback) {
+
+   var config=    {configuration:{
+                         publish: {
+                            callbacks: [
+                                {url : callbackUrl,
+                                 method : "POST",
+                                 include : [ "transcripts", "keywords", "topics", "metadata" ] }
+                            ]
+                         }
+                   } 
+               }; 
+   var metadata = {metadata : { external : {id : externalId} } }
+   var formData = { media: fs.createReadStream(file), configuration: JSON.stringify(config), metadata: JSON.stringify(metadata)};
+
+   request.post({
+         url: baseurl+'media',
+         json: true,
+         formData: formData,
+         headers: {'Authorization': 'Bearer '+bearerToken },
+         }, function(err, httpResponse, body) {
+               if (err) {
+                  return console.error('upload failed:', err);
+               }
+              console.log('Upload successful!  Server responded with:', body);
+              callback(err, res, body);
+        }
+    );
+}
+```
+
+```javascript
+app.post('/callback2', textParser, function(req, res){
+    data = JSON.parse(req.body);
+    x = data.media.metadata.external.id;
+    fileName = req.body.metadata.external.id;
+    fs.writeFile('public/'+fileName+'-en.srt', data.media.transcripts.srt, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    }); 
+    console.log('callback receievd with external id: '+fileName);
+});
+
+```
+
+
+```javascript
+// uses ffmpeg to extract the audio track (without transcoding) from a video file.  This will be more efficient then sending the entire video
+function extractAudio(scratchDir,fileName,res) {
+   var file = 'public/'+fileName;
+   var ext = file.substr(file.lastIndexOf('.') );
+   var audioFile = scratchDir+generateUUID()+ext;
+   var cmd = 'ffmpeg -y -i '+file+ ' -vn -acodec copy '+audioFile;
+   var child = exec(cmd, function(err, stdout, stderr) {
+        if (err !== null) {
+            return console.log('exec error: ' + err);
+        }  
+        externalId=fileName;
+        afterAudioExtracted(audioFile,externalId, res);
+   });
+}
+
+```
--- index.js
+++ index.js
@@ -2,54 +2,46 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
 var fs = require('fs');
-
 var request = require('request');
 var sys = require('sys')
 var exec = require('child_process').exec;
-
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var nconf = require('nconf');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Setup nconf to use (in-order): commandline, env, congig.json file
+nconf.argv()
+   .env()
+   .file({ file: 'config.json' });
+
+// create application/json parser 
+var jsonParser = bodyParser.json({type:'text/plain'});
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
+var textParser = bodyParser.text();
+var rawParser = bodyParser.raw();
+
 app.use(express.static('public'));
+app.set('view engine', 'jade');
 
+var bearerToken = nconf.get('vbtoken');
+var baseUrl = nconf.get('baseurl');
+var callback = nconf.get('callback');
+var scratchDir = nconf.get('scratchdir');
+//'https://apis.voicebase.com/v2-beta/'
+//'/tmp/';
 
-url = process.env.vbapiurl;
-key = process.env.vbkey;
-pass = process.env.vbpw;
-//var pass='letmeon';
-//var url='https://api.dev.voicebase.com/services';
-//var key='2A425147-7577-9542-3AED-4BCF806BB6EE';
-//var key='1586E668-F431-C9D6-D68B-88641B5C3EB8'
-//var pass='mrneutron';
-//var url=  "https://api.voicebase.com/services";
-var scratchDir = '/tmp/';
+console.log('baseurl: ' + baseUrl);
+console.log('vbtoken: ' + bearerToken);
+console.log('callback: ' + callback);
+console.log('scratchDir: ' + scratchDir);
 
 //Workaround to have thos global because I can not retriee the callback params :(
-var mediaId;
-var srt;
-var fileName;
+var callbackUrl = 'http://73.252.201.186:3000/callback2';
 
-
-//io.on('connection', function (socket) {
-  //socket.emit('news', { hello: 'world' });
-  //socket.on('my other event', function (data) {
-    //console.log(data);
-  //});
-//});
-
-
-//app.get('/', function(req, res){
-//  console.log(__dirname)
-//  res.sendFile(__dirname + '/index.html');
-//});
 function endsWith(str) {
     var suffix='.mp4';
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
-app.set('view engine', 'jade');
 app.get('/', function (req, res) {
   var files=fs.readdirSync('public');
   var filteredFiles = files.filter(endsWith);
@@ -57,51 +49,103 @@ app.get('/', function (req, res) {
 });
 
 
-
 app.get('/player', function (req, res) {
   var file = req.param('file');
   fileName = file;
-  console.log(file);
   res.render('player', { file:file });
 });
 
 
+//
+// Sequence of calls to trigger request for SRT, the results will be ready when POST is received on the callback url
+//  get("/cc")  calls
+//     requestSRT which calls extractAudio
+//     extractAudio() -- extracts audio from the video, using shell command to run ffmpeg, when it is done calls afterExtarctAudio function.
+//     afterExtractAudio() -- calls upload which does the POST, when then when POST is complete calls afterUpload function
+//  afterUpload() sends response to web client that did the get to /cc
+//
 app.get("/cc", requestSRT); 
-
 function requestSRT(req, res) {
-    var callbackUrl = req.protocol + '://' + req.get('host') + '/callback';
-    //var callbackUrl = 'https://' + req.get('host') + '/callback';
-    console.log(callbackUrl);
-    var file = 'public/'+fileName;
-    console.log(file);
-    //var audioFile = extractAudio(scratchDir,file);
-    uploadFile(file, callbackUrl,callbackUrl, res, afterUpload);
+    //var callbackUrl = req.protocol + '://' + req.get('host') + '/callback';
+    //var callbackUrl = 'http://73.252.201.186:3000/callback2';
+    //console.log(callbackUrl);
+    //console.log(fileName);
+    //TODO: Get the filename from the request (and not use the global var)
+    var audioFile = extractAudio(scratchDir,fileName,res);
 }
 
+// uses ffmpeg to extract the audio track (without transcoding) from a video file.  This will be more efficient then sending the entire video
+function extractAudio(scratchDir,fileName,res) {
+   var file = 'public/'+fileName;
+   var ext = file.substr(file.lastIndexOf('.') );
+   var audioFile = scratchDir+generateUUID()+ext;
+   var cmd = 'ffmpeg -y -i '+file+ ' -vn -acodec copy '+audioFile;
+   //console.log(cmd);
+   var child = exec(cmd, function(err, stdout, stderr) {
+        //console.log('stdout: ' + stdout);
+        //console.log('stderr: ' + stderr);
+        if (err !== null) {
+            return console.log('exec error: ' + err);
+        }  
+        externalId=fileName;
+        afterAudioExtracted(audioFile,externalId, res);
+   });
+}
+
+//after extraction is done, use REST api to upload the audio (note the callbackUrl to get notified when it is ready)
+function afterAudioExtracted(audioFile, externalId, res) {
+    uploadFileV2(audioFile,externalId, callbackUrl, res, afterUpload);
+}
+
+// after uploading, send the response back to web page, with the results from REST call 
 function afterUpload(err, res, data) {
+    //console.log(data);
     res.json(data);
 }
 
 
-app.post('/callback', function(req, res){
-    var d = new Date();
-    var n = d.toLocaleTimeString();
-    console.log('a callback was received at : ' + n);
-    //TODO:  Can not get the 3 post parameters.  Not sure why
-    //console.log('state : ' + req.body.state);
-    //console.log('mediaId : ' + req.body.mediaId);
-    //`console.log('time charged : ' + req.body.timeCharged);
+//
+// File upload functions (these examples use media file attachment, it is also possible to provide url to media file)
+//
 
-   // get the closed caption SRT file and write it to the public folder so it can be used in the video    
-   getSRT(mediaId);
+// v2 upload file function.
+//   note it posts the file to media along with optional configruation spec that includes thing such as the callback url, externalId, what processing is needed.
+//   if spec is not included, all default config  settings are used.
+function uploadFileV2(file, externalId, callbackUrl, res, callback) {
 
-});
+   var config=    {configuration:{
+                         publish: {
+                            callbacks: [
+                                {url : callbackUrl,
+                                 method : "POST",
+                                 include : [ "transcripts", "keywords", "topics", "metadata" ] }
+                            ]
+                         }
+                   } 
+               }; 
+   var metadata = {metadata : { external : {id : externalId} } }
+   var formData = { media: fs.createReadStream(file), configuration: JSON.stringify(config), metadata: JSON.stringify(metadata)};
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+   request.post({
+         url: baseurl+'media',
+         json: true,
+         formData: formData,
+         headers: {'Authorization': 'Bearer '+bearerToken },
+         }, function(err, httpResponse, body) {
+               if (err) {
+                  return console.error('upload failed:', err);
+               }
+              console.log('Upload successful!  Server responded with:', body);
+              //for (var prop in body ) {
+              //    console.log( prop + ' is ' + body[prop] );
+              //}
+             callback(err, res, body);
+        }
+    );
+}
 
-
+// v1 upload media function
+//   one difference with v2 is that all control parameters are passed in as form data whereas v2 has the config spec
 function uploadFile(file,callbackUrl,eCallbackUrl,res,callback) {
 
    var formData = {apiKey: key,
@@ -124,12 +168,14 @@ function uploadFile(file,callbackUrl,eCallbackUrl,res,callback) {
               for (var prop in body ) {
                   console.log( prop + ' is ' + body[prop] );
               }
-              mediaId=body.mediaId;
+              //callback function to do the the work when this processing is done
               callback(err, res, body);
         }
     );
 }
 
+
+// v1 function to retrieve the srt file given an mediaID
 function getSRT(mediaId) {
 
    var formData = {apiKey: key,
@@ -147,54 +193,48 @@ function getSRT(mediaId) {
                   return console.error('upload failed:', err);
                }
               console.log('Upload successful!  Server responded with:', body);
-              srt = body;
-              fs.writeFile('public/'+filename+'-en.srt', srt, function(err) {
+              fs.writeFile('public/'+fileName+'-en.srt', body, function(err) {
                   if(err) {
                       return console.log(err);
                   }
-                  console.log("The file was saved!");
               }); 
         }
     );
 }
 
-function extractAudio(scratchDir,file) {
-   var ext = file.substr(file.lastIndexOf('.') );
-   var audioFile = scratchDir+generateUUID()+ext;
-   var cmd = 'ffmpeg -y -i '+file+ ' -vn -acodec copy '+audioFile;
-   console.log(cmd);
-   var child;
-   child = exec(cmd, function (error, stdout, stderr) {
-      console.log('stdout: ' + stdout);
-      console.log('stderr: ' + stderr);
-      if (error !== null) {
-         console.log('exec error: ' + error);
-       }  
-   });
 
-  return audioFile;
-}
+//
+//Upload callbacks, url specified when media file is posted to media url
+//
 
-function uploadFile2(file,callback,eCallback) {
-  request.post({
-        url: url,
-        method: 'POST',
-        form: {apiKey: key,
-               password: pass,
-               version: '1.1',
-               action: 'uploadMedia',
-               transcriptType: 'machine-best',
-               //file:file,
-               file: fs.createReadStream(file),
-               machineReadyCallback:callback,
-               errorCallback:eCallback}
-        },function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body)
-            }
+//v2 callback code
+//   Note one difference with v1, is that v2 also can receive data (as specified in publish section of the configuration provide at media POST time)
+app.post('/callback2', textParser, function(req, res){
+    data = JSON.parse(req.body);
+    x = data.media.metadata.external.id;
+    //console.log(data.media.transcripts.srt);
+    //fileName = req.body.metadata.external.id;
+    fs.writeFile('public/'+fileName+'-en.srt', data.media.transcripts.srt, function(err) {
+        if(err) {
+            return console.log(err);
         }
-    );
-}
+    }); 
+    console.log('callback receievd with external id: '+fileName);
+});
+
+//v1 callback code
+//  Note one difference with v2, is that it has to make a request to get the srt data.  callback is just a notifier. 
+app.post('/callback', textParser, function(req, res){
+    console.log('state : ' + req.body.state);
+    console.log('mediaId : ' + req.body.mediaId);
+    console.log('time charged : ' + req.body.timeCharged);
+    console.log('externalId : ' + req.body.externalId);
+
+   // get the closed caption SRT file and write it to the public folder so it can be used in the video    
+   getSRT(req.body.mediaId);
+
+});
+
 
 function generateUUID(){
        var d = new Date().getTime();
@@ -206,4 +246,6 @@ function generateUUID(){
        return uuid;
 }
 
-
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
